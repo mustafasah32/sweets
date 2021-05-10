c701cbf10a52042b7de78abff57c51daf169b871
hoplaaa <hoplaaa>
2021-05-10 18:59:00 +0300

    used prime fw for cake components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
+  menu: MenuItem[]=[];
+
+
   title = 'sweets';
+
+  ngOnInit() {
+      this.menu = [
+          {
+              label:'File',
+              icon:'pi pi-fw pi-file',
+          }
+      ];
+  }
 }
--- src/app/cakes/cakes.module.ts
+++ src/app/cakes/cakes.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {ButtonModule} from 'primeng/button';
+import {PanelModule} from 'primeng/panel';
+import {DialogModule} from 'primeng/dialog';
+import {InputTextModule} from 'primeng/inputtext';
 
 import { CakesRoutingModule } from './cakes-routing.module';
 import { CakesComponent } from './cakes/cakes.component';
@@ -7,6 +13,11 @@ import { CakeComponent } from './cake/cake.component';
 import { CakesMockService } from './cakes-service/cakes-mock.service';
 import { CakesService } from './cakes-service/cakes.service';
 import { HttpClientModule } from '@angular/common/http';
+import {DataViewModule} from 'primeng/dataview';
+import {RatingModule} from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { RippleModule } from 'primeng/ripple';
 
 
 @NgModule({
@@ -16,6 +27,15 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   providers: [{provide:CakesService,useClass:CakesMockService}],
   imports: [
+    RatingModule,
+    DataViewModule,
+    PanelModule,
+    DialogModule,
+    DropdownModule,
+    InputTextModule,
+    ButtonModule,
+    RippleModule,
+    FormsModule,
     HttpClientModule,
     CommonModule,
     CakesRoutingModule
